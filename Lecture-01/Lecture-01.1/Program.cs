7355e1c07e12009f0c03be7011bfb914e2435fc6
Enes YALÇIN <107759141+eenesyalcin@users.noreply.github.com>
2023-10-25 21:44:26 +0300

    Lecture 1 Codes
    The codes related to the "First Application" lecture have been uploaded.

---
+++ Lecture-01/Lecture-01.1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_01._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // İLK UYGULAMA
+
+            Console.Write("Hello World");
+            Console.Read();
+        }
+    }
+}
