84f617a9dcdf8c2e5c218ebaf25c5d64f4bc3238
Enes YALÇIN <107759141+eenesyalcin@users.noreply.github.com>
2023-10-31 21:24:13 +0300

    Lecture 8 Codes
    The codes related to the "If-Else Structure-3" lecture have been uploaded.

---
+++ Lecture-08/Lecture-08.1/Lecture-08.1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_08._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // IF-ELSE YAPISI-3
+
+            string season;
+            Console.Write("Lütfen başharfi büyük olacak şekilde bir mevsim giriniz = ");
+            season = Console.ReadLine();
+            Console.WriteLine("\n--------------------------------------------------");
+
+            if(season == "Kış")
+            {
+                Console.WriteLine("*** KIŞ MEVSİMİNİN AYLARI ***");
+                Console.WriteLine("Aralık");
+                Console.WriteLine("Ocak");
+                Console.Write("Şubat");
+            }
+            else if(season == "İlkbahar")
+            {
+                Console.WriteLine("*** İLKBAHAR MEVSİMİNİN AYLARI ***");
+                Console.WriteLine("Mart");
+                Console.WriteLine("Nisan");
+                Console.Write("Mayıs");
+            }
+            else if (season == "Yaz")
+            {
+                Console.WriteLine("*** YAZ MEVSİMİNİN AYLARI ***");
+                Console.WriteLine("Haziran");
+                Console.WriteLine("Temmuz");
+                Console.Write("Ağustos");
+            }
+            else if (season == "Sonbahar")
+            {
+                Console.WriteLine("*** SONBAHAR MEVSİMİNİN AYLARI ***");
+                Console.WriteLine("Eylül");
+                Console.WriteLine("Ekim");
+                Console.Write("Kasım");
+            }
+            else
+            {
+                Console.Write("HATALI MEVSİM GİRİŞİ YAPTINIZ");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Lecture-08/Lecture-08.2/Lecture-08.2/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_08._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // IF-ELSE YAPISI-3
+
+            double price, discount, paidPrice;
+
+            Console.Write("Lütfen ürünlerin toplam tutarını giriniz = ");
+            price = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n--------------------------------------------------");
+
+            if(price > 0 && price <= 100)
+            {
+                discount = price * 10 / 100;
+                paidPrice = price - discount;
+                Console.WriteLine("İndirim tutarı = " + discount);
+                Console.Write("Ödenecek tutar = " + paidPrice);
+            }
+            else if(price > 100 && price <= 150)
+            {
+                discount = price * 15 / 100;
+                paidPrice = price - discount;
+                Console.WriteLine("İndirim tutarı = " + discount);
+                Console.Write("Ödenecek tutar = " + paidPrice);
+            }
+            else if (price > 150)
+            {
+                discount = price * 20 / 100;
+                paidPrice = price - discount;
+                Console.WriteLine("İndirim tutarı = " + discount);
+                Console.Write("Ödenecek tutar = " + paidPrice);
+            }
+            else
+            {
+                Console.Write("YANLIŞ TUTAR GİRDİNİZ");
+            }
+
+            Console.Read();
+        }
+    }
+}
