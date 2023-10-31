7bd769c00162742be7f7a4c352064b8b049e251c
Enes YALÇIN <107759141+eenesyalcin@users.noreply.github.com>
2023-10-31 21:10:21 +0300

    Lecture 7 Codes
    The codes related to the "If-Else Structure-2" lecture have been uploaded.

---
+++ Lecture-7/Lecture-07.1/Lecture-07.1/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_07._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // İF-ELSE YAPISI-2
+
+            int exam1, exam2, project, average;
+
+            Console.Write("Lütfen 1. sınav notunu giriniz = ");
+            exam1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Lütfen 2. sınav notunu giriniz = ");
+            exam2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Lütfen proje notunu giriniz = ");
+            project = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n--------------------------------------------------");
+
+            average = (exam1 + exam2 + project) / 3;
+
+            Console.WriteLine("Öğrencinin ders ortalaması = "+average);
+            
+            if(average >= 0 && average < 30)
+            {
+                Console.Write("Durum = ÇOK KÖTÜ");
+            }
+            else if(average >= 30 && average < 50)
+            {
+                Console.Write("Durum = KÖTÜ");
+            }
+            else if (average >= 50 && average < 75)
+            {
+                Console.Write("Durum = ORTA");
+            }
+            else if (average >= 75 && average < 90)
+            {
+                Console.Write("Durum = İYİ");
+            }
+            else if (average >= 90 && average <= 100)
+            {
+                Console.Write("Durum = ÇOK İYİ");
+            }
+            else
+            {
+                Console.Write("HATALI NOT GİRİŞİ YAPTINIZ");
+            }
+
+            Console.Read();
+        }
+    }
+}
