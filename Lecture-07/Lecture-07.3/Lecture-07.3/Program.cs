0c62c2afea337c1aad442ff4815852b2363dce95
Enes YALÇIN <107759141+eenesyalcin@users.noreply.github.com>
2023-10-31 21:23:06 +0300

    Lecture 7 Codes
    The codes related to the "If-Else Structure-2" lecture have been uploaded.

---
+++ Lecture-07/Lecture-07.3/Lecture-07.3/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_07._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // IF-ELSE YAPISI-2
+
+            int number;
+            Console.Write("Lütfen bir sayı giriniz = ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n--------------------------------------------------");
+
+            if (number % 2 == 0)
+            {
+                Console.Write("SAYI ÇİFTTİR");
+            }
+            else
+            {
+                Console.Write("SAYI TEKTİR");
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ Lecture-07/Lecture-07.4/Lecture-07.4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_07._4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // IF-ELSE YAPISI-2
+
+            int number;
+            Console.Write("Lütfen bir sayı giriniz = ");
+            number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("\n--------------------------------------------------");
+
+            if (number % 5 == 0)
+            {
+                Console.Write("SAYI 5 SAYISINA TAM BÖLÜNÜR");
+            }
+            else
+            {
+                Console.Write("SAYI 5 SAYISINA TAM BÖLÜNMEZ");
+            }
+
+            Console.Read();
+        }
+    }
+}
